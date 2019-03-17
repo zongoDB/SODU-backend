298c527e74d2b70fda27881c2954d117aa502db0
jamalissah <issahjamal0@gmail.com>
2019-03-17 18:55:22 +0000

    Added GPS function

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
+var GPS = require('gps');
+var gps = new GPS;
+
+
 admin.initializeApp(functions.config().firebase);
 
 var db = admin.firestore();
@@ -14,5 +18,24 @@ var db = admin.firestore();
  *                     More info: https://expressjs.com/en/api.html#res
  */
 exports.soduHttp = (req, res) => {
-  res.send('ok');
+  gps_data = req.body.gps;
+  seats = req.body.seats;
+  number_plate = req.body.plate;
+  owner_id = req.body.owner;
+
+  gps.on('data', function(data){
+
+    var data = {
+
+    seats: req.body.seats,
+    timestamp: admin.firestore.Timestamp.fromDate(data.time),
+    geopoint: new admin.firestore.GeoPoint(data.lat,data.lon),
+
+    }
+
+    db.collection('users').doc(owner_id).collection('cars').doc(number_plate).update(data);
+    res.send('ok');
+
+  });
+  gps.update(gps_data);
 };
